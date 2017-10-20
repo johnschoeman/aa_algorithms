8dc0fc97cb02c7043bc8c9272d1a57884b52d9d9
John Schoeman <johnschoeman1617@gmail.com>
2017-10-20 08:23:42 -0700

    Init w12d4 algorithm

--- W12D3/john_schoeman/lib/k_largest_elements.rb
+++ W12D3/john_schoeman/lib/k_largest_elements.rb
@@ -1,5 +1,7 @@
 require_relative 'heap'
 
+# using a max heap
+# O(nlog(n-k))
 def k_largest_elements(array, k)
   heap = BinaryMinHeap.new { |a,b| b <=> a }
   result = []
@@ -11,4 +13,18 @@ def k_largest_elements(array, k)
   end
 
   result
+end
+
+# using a min heap
+# O(nlogk)
+def k_largest_elements2(array, k) 
+  result = BinaryMinHeap.new
+  k.times do
+    result.push(array.pop)
+  end
+  until array.empty?
+    result.push(array.pop)
+    result.extract
+  end
+  result.store
 end
