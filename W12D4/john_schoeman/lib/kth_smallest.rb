df34cf8d8a1e4128b02940e2cf9b6942ac210e83
John Schoeman <johnschoeman1617@gmail.com>
2017-10-22 14:42:11 -0700

    Init w12d5 bst algorithm

--- W12D4/john_schoeman/lib/kth_smallest.rb
+++ W12D4/john_schoeman/lib/kth_smallest.rb
@@ -1,5 +1,28 @@
 require_relative 'quick_sort'
 
-def kth_smallest(array)
+class Array
+  def kth_smallest(k)
 
+  end
 end
+
+k = 3
+[9,1,2,8,3,7,4,5,6]
+
+# pick pivot and do one partition step
+# pick partition that includes index k - 1
+# return when partion index places something in k (returns partion index == k)
+# on average each step will have the array size
+
+[1,2,8,3,7,4,5,6,9]
+[---------------]
+[1,2,8,3,7,4,5,6,9]
+  [-------------]
+[1,2,8,3,7,4,5,6,9]
+    [-----------]
+[1,2,3,7,4,5,6,8,9]
+      [---------]
+[1,2,3,4,5,6,7,8,9]
+      [-----]
+
+# quadtree - collision detection
